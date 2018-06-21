3598712a7db24ec929e1c9d9885cf5469e2c630e
machineL1994 <sqzhang1994@gmail.com>
2018-06-21 15:48:30 +0800

    some works have done

---
+++ src/main/java/com/sqzhang/common/Const.java
@@ -0,0 +1,11 @@
+package com.sqzhang.common;
+
+public class Const {
+    public static final String CURRENT_USER = "currentUser";
+    public interface Role{
+        int Role_CUSTOMER = 0;//普通用户
+        int Role_ADMIN = 1;//管理员
+    }
+    public static final String EAMIL = "eamil";
+    public static final String USERNAME = "username";
+}
